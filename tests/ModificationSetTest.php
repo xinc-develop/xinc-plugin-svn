acdbdde7a44372893255ca43532451891689a482
Sebastian B. Knapp <rock@ccls-online.de>
2016-01-06 00:54:01 +0100

    pre svn relocate - WIP

--- tests/ModificationSetTest.php
+++ tests/ModificationSetTest.php
@@ -22,6 +22,7 @@
  *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+use Xinc\Core\Build\BuildInterface;
 use Xinc\Core\Config\Config;
 use Xinc\Core\Project\Project;
 use Xinc\Core\Project\Status as ProjectStatus;
@@ -32,6 +33,44 @@ use Xinc\Core\Test\BaseTest;
  */
 class TestModificationSet extends BaseTest
 {  
+	public function getSvnRoot()
+	{
+		exec('svn info --xml',$xml);
+		$xml = simplexml_load_string(join('',$xml));
+		$root = $xml->xpath('/info/entry/repository/root');
+		
+		return "{$root[0]}";
+	}
+		
+	public function getSvnVersion()
+	{
+		exec("svn --version --quiet",$out);
+		return $out[0];
+	}
+	
+	public function relocateRepo($wc,$repo)
+	{
+		$current = getcwd();
+		chdir($wc);
+		$this->getSvnRoot();
+	    if(version_compare('1.7.0',$this->getSvnVersion(),'<=')) {
+			$old = $this->getSvnRoot();
+			exec("svn switch --relocate $old $repo");	
+		}
+		else {
+			exec("svn relocate $repo");
+		}
+		chdir($current);
+	}
+	
+	public function setUp()
+	{
+	    $one = __DIR__ . '/working-copy/one';
+	    $repo = __DIR__ . '/repos/one';
+	    
+	    $this->relocateRepo($one,$repo);
+	}
+	
 	public function testProjectFromConfig1()
 	{
 		$conf = $this->defaultConfig();
@@ -42,8 +81,9 @@ class TestModificationSet extends BaseTest
 	    $build2 = $this->aBuildWithConfig($conf);
 	    $project = $reg->getProject("ProjectSvn");
 	    $this->assertInstanceOf('Xinc\Core\Project\Project',$project);
-	    
 	    $build2->process(Slot::PRE_PROCESS);
+	    
+	    $this->assertEquals($build2->getStatus(),BuildInterface::PASSED);
 	    
 	    return;
 	    $iterator = $reg->getProjectIterator();
